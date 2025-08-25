02221875e37f2b040f94054f758c707ffb7c8e56
retsu14 <tajanlangitelieser19@gmail.com>
2025-08-25 22:41:01 +0800

    wip

--- app/threads/[slug]/page.tsx
+++ app/threads/[slug]/page.tsx
@@ -1,12 +1,8 @@
 import ChatComponent from "@/components/Chat";
 
 const page = async ({ params }: { params: { slug: string } }) => {
-  const { slug } = await params;
-  return (
-    <div>
-      <ChatComponent />
-    </div>
-  );
+  // const { slug } = await params;
+  return <div>{/* <ChatComponent /> */}</div>;
 };
 
 export default page;
